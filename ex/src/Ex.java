31754e82e22f11ff9e0035232dc6ba506b93937b
alfredocardos <alfredohenrique63@outlook.com>
2022-05-13 23:12:56 -0300

    Exercicios

---
+++ ex/src/Ex.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String T = scan.nextLine();
+        String respostasCompetidores = scan.nextLine();
+
+        String[] respostasCompetidoresSplit = respostasCompetidores.split(" ");
+//TODO: Complete o código com uma possível solução para o problema.
+        int count = 0;
+        for (int i = 0; i < respostasCompetidoresSplit.length; i++ ) {
+            if (T.equals(respostasCompetidoresSplit[i]))
+                count++;
+        }
+
+        System.out.println(count);
+    }
+    }
+}
